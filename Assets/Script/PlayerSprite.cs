6477c7836823cda14d24c1e2efd73ca1f063c373
Roslairy <wpikuy@qq.com>
2014-11-30 21:28:36 +0800

    After completing player animation

---
+++ Assets/Script/PlayerSprite.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Policy;
+using UnityEngine;
+using System.Collections;
+
+public class PlayerSprite : MonoBehaviour {
+
+    // for animation
+    void step(){
+        // TODO: 脚步声
+    }
+
+    // for Player
+
+    public AnimationClip[] clips;
+
+    public enum Anim{
+        idle,
+        walk,
+        run,
+        jump
+    }
+
+    public void play(Anim type){
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
